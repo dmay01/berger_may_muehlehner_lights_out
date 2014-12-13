b7c0a93704b3b5144352dc79ac77af5484b8e15c
Benedikt Berger <bberger@student.tgm.ac.at>
2014-12-13 14:14:47 +0100

    Small extensions

--- LightsPanel.java
+++ LightsPanel.java
@@ -3,15 +3,22 @@ package berger_may_muehlehner_lights_out;
 import javax.swing.JPanel;
 
 /**
+ * Optische Oberflaeche des Programms
  * 
- * 
- * @author
- * @version
+ * @author benedikt berger
+ * @version 1.0
  * 
  */
 public class LightsPanel extends JPanel {
 
+	private static int amount = 3;
 	private static final long serialVersionUID = 1L;
+	private JPanel buttons, bottom;
 
-	// TODO
+	public LightsPanel(LightsController controller){
+		buttons = new JPanel();
+		bottom = new JPanel();
+		
+		
+	}
 }
